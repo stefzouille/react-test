93bae30612c38ea933a6f85503ccc0fd4f1433fc
stefzouille <contact.lokcy@gmail.com>
2021-04-06 20:08:49 +0200

    fff

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
 
 function App() {
+  
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
+        <div>
+          <button type="button" onClick={() => alert("Hello")}>
+            <div/>
+            <p>Vous avez cliqué {count} fois</p>
+      <button onClick={() => setCount(count + 1)}>
+        Cliquez ici
+      </button>
+      Click me
+    </button>
+        </div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          test button 
         </p>
+        <div>
+      <p>Vous avez cliqué {count} fois</p>
+      <button onClick={() => setCount(count + 1)}>
+        Cliquez ici
+      </button>
+    </div>
         <a
           className="App-link"
           href="https://reactjs.org"
